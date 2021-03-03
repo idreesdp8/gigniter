5467840496b7a9e4997d130a850c34dd0c9ee6cc
HamzaZafarBhatti <hamza0952454@gmail.com>
2021-03-03 16:00:34 +0500

    checkout functionality

---
+++ mail.php
@@ -0,0 +1,9 @@
+<?php
+$to = "hamza0952454@gmail.com";
+$subject = "My subject";
+$txt = "Hello world!";
+$headers = "From: webmaster@example.com" . "\r\n" .
+"CC: somebodyelse@example.com";
+
+mail($to,$subject,$txt,$headers);
+?>
