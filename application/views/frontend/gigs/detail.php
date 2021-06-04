9ee8313f1383fac78a19242d936a9435a7bd7e7a
HamzaZafarBhatti <hamza0952454@gmail.com>
2021-06-04 11:53:21 +0500

    changes

--- application/views/frontend/gigs/detail.php
+++ application/views/frontend/gigs/detail.php
@@ -191,11 +191,10 @@
                     </div>
                     <div class="mb-3">
                         <ul class="social-share">
-                            <li><a href="#0"><i class="fab fa-facebook-f"></i></a></li>
-                            <li><a href="#0"><i class="fab fa-twitter"></i></a></li>
-                            <li><a href="#0"><i class="fab fa-pinterest-p"></i></a></li>
-                            <li><a href="#0"><i class="fab fa-linkedin-in"></i></a></li>
-                            <li><a href="#0"><i class="fab fa-google-plus-g"></i></a></li>
+                            <li><a href="http://www.facebook.com/sharer.php?u=<?php echo user_base_url().'gigs/detail?gig='.$gig->id ?>"><i class="fab fa-facebook-f"></i></a></li>
+                            <li><a href="http://twitter.com/share?url=<?php echo user_base_url().'gigs/detail?gig='.$gig->id ?>"><i class="fab fa-twitter"></i></a></li>
+                            <li><a href="http://pinterest.com/pin/create/button/?url=<?php echo user_base_url().'gigs/detail?gig='.$gig->id ?>"><i class="fab fa-pinterest-p"></i></a></li>
+                            <li><a href="http://www.linkedin.com/shareArticle?mini=true&url=<?php echo user_base_url().'gigs/detail?gig='.$gig->id ?>"><i class="fab fa-linkedin-in"></i></a></li>
                         </ul>
                     </div>
                     <?php
