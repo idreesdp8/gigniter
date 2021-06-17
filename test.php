c3627f17526a9c63e8f7382bd3e7c44f7287bab4
HamzaZafarBhatti <hamza0952454@gmail.com>
2021-06-17 07:44:28 +0500

    changes

--- test.php
+++ test.php
@@ -1,7 +1,12 @@
 <?php 
 
-$curdate = 'SELECT CURDATE()';
-$query = $this->db->query($sql);
-		echo $query->row();
-// $date =  new DateTime;
-echo json_encode($curdate);
+// $curdate = 'SELECT CURDATE()';
+// $query = $this->db->query($sql);
+// 		echo $query->row();
+// // $date =  new DateTime;
+// echo json_encode($curdate);
+
+// if()
+
+mkdir(user_asset_url().'session');
+mkdir(user_asset_url().'session/thumb');
