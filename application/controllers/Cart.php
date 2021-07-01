08fed3fceab9dea33fc4940cd5d38eb271cc6255
younasali22@gmail.com <younasali22@gmail.com>
2021-07-01 14:36:13 +0500

    younas changes uploaded

--- application/controllers/Cart.php
+++ application/controllers/Cart.php
@@ -440,14 +440,13 @@ class Cart extends CI_Controller
 
 				$file_name = 'ticket_' . $gig_ticket_qr_token . '.pdf';
 				$html_code = $this->load->view('frontend/bookings/download_tickets', $datas, TRUE);
-			 
-				
-				$options = new Options();
-				$options->setIsRemoteEnabled(true); 
-				$pdf = new Dompdf\Dompdf($options);  
+			  
+				//$options = new Options();
+				//$options->setIsRemoteEnabled(true); 
+				$pdf = new Dompdf\Dompdf();  
 				$pdf->loadHtml($html_code);
 				$pdf->render();
-				$file = $pdf->output();
+				$file = $pdf->output(['isRemoteEnabled' => true]);
 				file_put_contents("downloads/tickets_qr_code_imgs/$file_name", $file);   
 
 				if (strlen($gig_ticket_qr_token) > 0) {
