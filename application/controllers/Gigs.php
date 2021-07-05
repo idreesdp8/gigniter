5373d6e0c876fbe615cdad93949d14b386616a39
HamzaZafarBhatti <hamza0952454@gmail.com>
2021-07-05 11:33:30 +0500

    changes

--- application/controllers/Gigs.php
+++ application/controllers/Gigs.php
@@ -540,7 +540,7 @@ class Gigs extends CI_Controller
 			if (isset($this->dbs_user_id)) {
 				$user = $this->users_model->get_user_by_id($this->dbs_user_id);
 				$stripe_details = $this->users_model->get_user_stripe_details($user->id);
-				$user->stripe = $stripe_details->stripe_id;
+				$user->stripe = $stripe_details->stripe_id ?? '';
 				$data['user'] = $user;
 			}
 			$data['countries'] = $this->countries_model->get_all_countries();
