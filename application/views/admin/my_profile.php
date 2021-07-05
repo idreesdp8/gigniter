bb1efab54e342af727abef2e091c5046abb839da
younasali22@gmail.com <younasali22@gmail.com>
2021-07-05 17:52:38 +0500

    younas changes uploaded

--- application/views/admin/my_profile.php
+++ application/views/admin/my_profile.php
@@ -23,71 +23,74 @@
         <div class="card-header header-elements-inline">
           <h5 class="card-title">Update Profile</h5>
         </div>  
+		<div class="card-body"> 
 		<form name="datas_form" id="datas_form" method="post" action="<?php echo site_url('admin/settings/my_profile'); ?>" class="form-horizontal" enctype="multipart/form-data">
-		  <div class="form-group">
-			<label class="col-md-2 control-label" for="fname">First Name <span class="reds">*</span></label>
-			<div class="col-md-6">
-			  <input name="fname" id="fname" type="text" class="form-control" value="<?php echo (isset($row)) ? $row->fname : set_value('fname'); ?>" data-error="#fname1">
-			  <span id="fname1" class="text-danger"><?php echo form_error('fname'); ?></span> </div>
-		  </div>
-		  <div class="form-group">
-			<label class="col-md-2 control-label" for="lname">Last Name <span class="reds">*</span></label>
-			<div class="col-md-6">
-			  <input name="lname" id="lname" type="text" class="form-control" value="<?php echo (isset($row)) ? $row->lname : set_value('lname'); ?>" data-error="#lname1">
-			  <span id="name1" class="text-danger"><?php echo form_error('lname'); ?></span> </div>
-		  </div>
-		  <div class="form-group">
-			<label class="col-md-2 control-label" for="email">Email <span class="reds">*</span></label>
-			<div class="col-md-6">
-			  <input name="email" id="email" type="text" class="form-control" value="<?php echo (isset($row)) ? $row->email : set_value('email'); ?>" readonly disabled="disabled">
+		 
+			<div class="row">
+				<div class="col-md-6"> 
+				  <div class="form-group">
+					<label for="fname">First Name: </label>
+					<input name="fname" id="fname" type="text" class="form-control" value="<?php echo (isset($row)) ? $row->fname : set_value('fname'); ?>" data-error="#fname1">
+					<span id="fname1" class="text-danger"><?php echo form_error('fname'); ?></span>
+				  </div>
+				  <div class="form-group">
+					<label for="lname">Last Name <span class="reds"> </span></label>
+					 <input name="lname" id="lname" type="text" class="form-control" value="<?php echo (isset($row)) ? $row->lname : set_value('lname'); ?>" data-error="#lname1">
+				  <span id="name1" class="text-danger"><?php echo form_error('lname'); ?></span>
+				  </div>   
+				  
+				  <div class="form-group">
+					<label  for="email">Email <span class="reds">*</span> </label>
+					<input name="email" id="email" type="text" class="form-control" value="<?php echo (isset($row)) ? $row->email : set_value('email'); ?>" readonly disabled="disabled">
+					<span id="email1" class="text-danger"><?php echo form_error('email'); ?></span>
+				  </div>
+				  <div class="form-group">
+					<label for="phone_no">Phone No <span class="reds">*</span></label>
+					  <input name="phone_no" id="phone_no" type="text" class="form-control" value="<?php echo (isset($row)) ? $row->phone_no : set_value('phone_no'); ?>" data-error="#phone_no1">
+				  	  <span id="phone_no1" class="text-danger"><?php echo form_error('phone_no'); ?></span> 
+				  </div>  
+				  
+				   <div class="form-group">
+					<label for="mobile_no">Mobile No <span class="reds">*</span></label>
+					 <input name="mobile_no" id="mobile_no" type="text" class="form-control" value="<?php echo (isset($row)) ? $row->mobile_no : set_value('mobile_no'); ?>" data-error="#mobile_no1">
+				  <span id="mobile_no1" class="text-danger"><?php echo form_error('mobile_no'); ?></span> 
+				  </div>  
+				</div> 
+				
+				 <div class="col-md-6"> 
+				  <div class="form-group">
+					<label for="description">Description</label></label>
+					 <textarea name="description" id="description" class="form-control" rows="5" data-error="#description1"><?php echo (isset($row)) ? $row->description : set_value('description'); ?></textarea>
+				  <span class="text-danger"><?php echo form_error('description'); ?></span> 
+				  </div>
+				  <div class="form-group">
+					<label  for="address">Address <span class="reds">*</span></label>
+					 <textarea name="address" id="address" class="form-control" rows="5" data-error="#address1"><?php echo (isset($row)) ? $row->address : set_value('address'); ?></textarea>
+				  <span id="address1" class="text-danger"><?php echo form_error('address'); ?></span>
+				  </div> 
+				  
+				  <div class="form-group">
+					<label for="image">Picture <span class="reds">*</span></label>
+					<input type="file" name="image" id="image" class="file-styled" data-error="#image1">
+					<?php if(isset($row->image) && strlen($row->image)>0){ ?>
+					<input type="hidden" name="old_image" id="old_image" value="<?php echo $row->image; ?>">
+					<?php echo '( '.$row->image.'	)'; } ?> <span id="image1" class="text-danger">
+					<?php 
+						echo form_error('image'); 
+						if(isset($_SESSION['prof_img_error'])){
+							echo $_SESSION['prof_img_error'];
+						} ?>
+					</span>  
+				   
+				  </div> 
+				</div>
 			</div>
-		  </div>
-		  <div class="form-group">
-			<label class="col-md-2 control-label" for="phone_no">Phone No <span class="reds">*</span></label>
-			<div class="col-md-6">
-			  <input name="phone_no" id="phone_no" type="text" class="form-control" value="<?php echo (isset($row)) ? $row->phone_no : set_value('phone_no'); ?>" data-error="#phone_no1">
-			  <span id="phone_no1" class="text-danger"><?php echo form_error('phone_no'); ?></span> </div>
-		  </div>
-		  <div class="form-group">
-			<label class="col-md-2 control-label" for="mobile_no">Mobile No <span class="reds">*</span></label>
-			<div class="col-md-6">
-			  <input name="mobile_no" id="mobile_no" type="text" class="form-control" value="<?php echo (isset($row)) ? $row->mobile_no : set_value('mobile_no'); ?>" data-error="#mobile_no1">
-			  <span id="mobile_no1" class="text-danger"><?php echo form_error('mobile_no'); ?></span> </div>
-		  </div>
-		  <div class="form-group">
-			<label class="col-md-2 control-label" for="description">Description</label>
-			<div class="col-md-6"> 
-			   <textarea name="description" id="description" class="form-control" rows="5" data-error="#description1"><?php echo (isset($row)) ? $row->description : set_value('description'); ?></textarea>
-			  <span class="text-danger"><?php echo form_error('description'); ?></span> 
-			  </div>
-		  </div>
-		  <div class="form-group">
-			<label class="col-md-2 control-label" for="image">Picture</label>
-			<div class="col-md-6">
-			  <input type="file" name="image" id="image" class="file-styled" data-error="#image1">
-			  <?php if(isset($row->image) && strlen($row->image)>0){ ?>
-			  <input type="hidden" name="old_image" id="old_image" value="<?php echo $row->image; ?>">
-			  <?php echo '( '.$row->image.'	)'; } ?> <span id="image1" class="text-danger">
-			  <?php 
-					echo form_error('image'); 
-					if(isset($_SESSION['prof_img_error'])){
-						echo $_SESSION['prof_img_error'];
-					} ?>
-			  </span> </div>
-		  </div>
-		  <div class="form-group">
-			<label class="col-md-2 control-label" for="address">Address <span class="reds">*</span></label>
-			<div class="col-md-6">
-			  <textarea name="address" id="address" class="form-control" rows="5" data-error="#address1"><?php echo (isset($row)) ? $row->address : set_value('address'); ?></textarea>
-			  <span id="address1" class="text-danger"><?php echo form_error('address'); ?></span> </div>
-		  </div>
-		  <div class="form-group">
-			<label class="col-md-2 control-label"></label>
-			<div class="col-md-6">
-			  <button class="btn border-slate text-slate-800 btn-flat" type="submit" name="updates" id="updates"><i class="glyphicon glyphicon-ok position-left"></i>Update Profile</button>
-			</div>
-		  </div>
+  		 
+			 <div class="text-right">
+				<button type="submit" name="updates" id="updates" class="btn btn-primary">Update Profile <i class="icon-paperplane ml-2"></i> </button>
+			  </div>  
 		</form>  
+		</div>
       </div>
       <!-- /basic layout -->
     </div>
