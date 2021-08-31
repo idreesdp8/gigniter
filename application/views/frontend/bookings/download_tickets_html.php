889621996fce7a6ceab29acbb1b84b38cc5ae834
flopmart <34133163+asadnisar98@users.noreply.github.com>
2021-08-31 13:23:25 +0500

    Update download_tickets_html.php

--- application/views/frontend/bookings/download_tickets_html.php
+++ application/views/frontend/bookings/download_tickets_html.php
@@ -14,36 +14,299 @@
     </style>
 </head>
 <body>
-    <div style="height: 25em; border: .15em solid black; border-radius: 4.5em; display: flex; width: 100%; position: relative;">
-        <div style="border-radius: 4em; overflow: hidden; height: -webkit-fill-available;">
-            <img src="<?php echo downloads_url() ?>gig8.jpg" alt="" style="width: 25em; height: 25em; object-fit: cover; border-radius: 4em;">
-        </div>
-        <div>
-            <div style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);">
-                <div>
-                    <div style="font-size: 3em;text-transform: uppercase;">Title of concert</div>
-                    <div style="font-size: 1.5em;text-transform: capitalize;">subtitle</div>
-                    <div style="font-size: 1.5em;text-transform: capitalize;">from <span style="font-weight: 600;text-transform: uppercase;">02:00 pm</span> to <span style="font-weight: 600;text-transform: uppercase;">04:00 pm</span></div>
+    <div style="border: .15em solid black;border-radius:10px;  width: 100%; position: relative; margin-bottom:20px; ">
+            <div style="display:inline-block; width:100%;">
+                <div style="width:35%;float: left;">
+                    <img src="<?php echo downloads_url() ?>gig8.jpg" alt="" style="width: 100%; margin: 5px;">
                 </div>
-                <div>
-                    <div style="font-size: 2em;text-transform: uppercase;font-weight: 600;">name of purchaser</div>
-                    <div style="font-size: 2em;text-transform: capitalize;font-weight: 600;">paid: <span>$38.76</span></div>
-                </div>
-                <div>
-                    <div style="font-size: 1.5em;text-transform: capitalize;">Venue: <span>ABCD Street</span></div>
-                </div>
-            </div>
-            <div style="position: absolute; right: 10%; top: 50%; transform: translate(0, -50%); text-align: center;">
-                <div style="width: 13em;height: 10em;border: .25em solid black;position: flex;display: flex;justify-content: center;align-items: center;flex-direction: column;">
-                    <div style="font-size: 1.5em;">September</div>
-                    <div style="font-size: 2.5em; font-weight: 700;">01</div>
-                    <div style="font-size: 1.5em;">2021</div>
+                <div style="width: 45%; margin-left:20px; float: left;">
+                        <div>
+                            <div style="font-size: 2em;text-transform: uppercase;">Title of concert</div>
+                            <div style="font-size: 1em;text-transform: capitalize;">subtitle</div>
+                            <div style="font-size: 1em;text-transform: capitalize;">from <span style="font-weight: 600;text-transform: uppercase;">02:00 pm</span> to <span style="font-weight: 600;text-transform: uppercase;">04:00 pm</span></div>
+                         </div>  
+                         <div style="margin-top:30px;">
+                            <div style="font-size: 1.5em;text-transform: uppercase;font-weight: 600;">name of purchaser</div>
+                             <div style="font-size: 1.5em;text-transform: capitalize;font-weight: 600;">paid: <span>$38.76</span></div>
+                         </div>
+                 </div>
+                           
+
+                <div style="width:15%;float: right;margin:5px 10px 0px 10px">
+                <div style=" text-align: center;">
+                <div style="width: 7em;height: 5em;border: 2px solid black;position: flex;display: flex;justify-content: center;align-items: center;flex-direction: column;">
+                    <div style="font-size: 1em;">September</div>
+                    <div style="font-size: 1.5em; font-weight: 700;">01</div>
+                    <div style="font-size: 1em;">2021</div>
                 </div>
                 <div>
                     <img src="<?php echo downloads_url() ?>ticket_6123940874306.png" alt="">
                 </div>
             </div>
-        </div>
+                </div>
+
+
+            </div>
+            
     </div>
+
+
+    <div style="border: .15em solid black;border-radius:10px;  width: 100%; position: relative; margin-bottom:20px; ">
+            <div style="display:inline-block; width:100%;">
+                <div style="width:35%;float: left;">
+                    <img src="<?php echo downloads_url() ?>gig8.jpg" alt="" style="width: 100%; margin: 5px;">
+                </div>
+                <div style="width: 45%; margin-left:20px; float: left;">
+                        <div>
+                            <div style="font-size: 2em;text-transform: uppercase;">Title of concert</div>
+                            <div style="font-size: 1em;text-transform: capitalize;">subtitle</div>
+                            <div style="font-size: 1em;text-transform: capitalize;">from <span style="font-weight: 600;text-transform: uppercase;">02:00 pm</span> to <span style="font-weight: 600;text-transform: uppercase;">04:00 pm</span></div>
+                         </div>  
+                         <div style="margin-top:30px;">
+                            <div style="font-size: 1.5em;text-transform: uppercase;font-weight: 600;">name of purchaser</div>
+                             <div style="font-size: 1.5em;text-transform: capitalize;font-weight: 600;">paid: <span>$38.76</span></div>
+                         </div>
+                 </div>
+                           
+
+                <div style="width:15%;float: right;margin:5px 10px 0px 10px">
+                <div style=" text-align: center;">
+                <div style="width: 7em;height: 5em;border: 2px solid black;position: flex;display: flex;justify-content: center;align-items: center;flex-direction: column;">
+                    <div style="font-size: 1em;">September</div>
+                    <div style="font-size: 1.5em; font-weight: 700;">01</div>
+                    <div style="font-size: 1em;">2021</div>
+                </div>
+                <div>
+                    <img src="<?php echo downloads_url() ?>ticket_6123940874306.png" alt="">
+                </div>
+            </div>
+                </div>
+
+
+            </div>
+            
+    </div>
+
+
+    <div style="border: .15em solid black;border-radius:10px;  width: 100%; position: relative; margin-bottom:20px; ">
+            <div style="display:inline-block; width:100%;">
+                <div style="width:35%;float: left;">
+                    <img src="<?php echo downloads_url() ?>gig8.jpg" alt="" style="width: 100%; margin: 5px;">
+                </div>
+                <div style="width: 45%; margin-left:20px; float: left;">
+                        <div>
+                            <div style="font-size: 2em;text-transform: uppercase;">Title of concert</div>
+                            <div style="font-size: 1em;text-transform: capitalize;">subtitle</div>
+                            <div style="font-size: 1em;text-transform: capitalize;">from <span style="font-weight: 600;text-transform: uppercase;">02:00 pm</span> to <span style="font-weight: 600;text-transform: uppercase;">04:00 pm</span></div>
+                         </div>  
+                         <div style="margin-top:30px;">
+                            <div style="font-size: 1.5em;text-transform: uppercase;font-weight: 600;">name of purchaser</div>
+                             <div style="font-size: 1.5em;text-transform: capitalize;font-weight: 600;">paid: <span>$38.76</span></div>
+                         </div>
+                 </div>
+                           
+
+                <div style="width:15%;float: right;margin:5px 10px 0px 10px">
+                <div style=" text-align: center;">
+                <div style="width: 7em;height: 5em;border: 2px solid black;position: flex;display: flex;justify-content: center;align-items: center;flex-direction: column;">
+                    <div style="font-size: 1em;">September</div>
+                    <div style="font-size: 1.5em; font-weight: 700;">01</div>
+                    <div style="font-size: 1em;">2021</div>
+                </div>
+                <div>
+                    <img src="<?php echo downloads_url() ?>ticket_6123940874306.png" alt="">
+                </div>
+            </div>
+                </div>
+
+
+            </div>
+            
+    </div>
+
+    <div style="border: .15em solid black;border-radius:10px;  width: 100%; position: relative; margin-bottom:20px; ">
+            <div style="display:inline-block; width:100%;">
+                <div style="width:35%;float: left;">
+                    <img src="<?php echo downloads_url() ?>gig8.jpg" alt="" style="width: 100%; margin: 5px;">
+                </div>
+                <div style="width: 45%; margin-left:20px; float: left;">
+                        <div>
+                            <div style="font-size: 2em;text-transform: uppercase;">Title of concert</div>
+                            <div style="font-size: 1em;text-transform: capitalize;">subtitle</div>
+                            <div style="font-size: 1em;text-transform: capitalize;">from <span style="font-weight: 600;text-transform: uppercase;">02:00 pm</span> to <span style="font-weight: 600;text-transform: uppercase;">04:00 pm</span></div>
+                         </div>  
+                         <div style="margin-top:30px;">
+                            <div style="font-size: 1.5em;text-transform: uppercase;font-weight: 600;">name of purchaser</div>
+                             <div style="font-size: 1.5em;text-transform: capitalize;font-weight: 600;">paid: <span>$38.76</span></div>
+                         </div>
+                 </div>
+                           
+
+                <div style="width:15%;float: right;margin:5px 10px 0px 10px">
+                <div style=" text-align: center;">
+                <div style="width: 7em;height: 5em;border: 2px solid black;position: flex;display: flex;justify-content: center;align-items: center;flex-direction: column;">
+                    <div style="font-size: 1em;">September</div>
+                    <div style="font-size: 1.5em; font-weight: 700;">01</div>
+                    <div style="font-size: 1em;">2021</div>
+                </div>
+                <div>
+                    <img src="<?php echo downloads_url() ?>ticket_6123940874306.png" alt="">
+                </div>
+            </div>
+                </div>
+
+
+            </div>
+            
+    </div>
+
+
+    <div style="border: .15em solid black;border-radius:10px;  width: 100%; position: relative; margin-bottom:20px; ">
+            <div style="display:inline-block; width:100%;">
+                <div style="width:35%;float: left;">
+                    <img src="<?php echo downloads_url() ?>gig8.jpg" alt="" style="width: 100%; margin: 5px;">
+                </div>
+                <div style="width: 45%; margin-left:20px; float: left;">
+                        <div>
+                            <div style="font-size: 2em;text-transform: uppercase;">Title of concert</div>
+                            <div style="font-size: 1em;text-transform: capitalize;">subtitle</div>
+                            <div style="font-size: 1em;text-transform: capitalize;">from <span style="font-weight: 600;text-transform: uppercase;">02:00 pm</span> to <span style="font-weight: 600;text-transform: uppercase;">04:00 pm</span></div>
+                         </div>  
+                         <div style="margin-top:30px;">
+                            <div style="font-size: 1.5em;text-transform: uppercase;font-weight: 600;">name of purchaser</div>
+                             <div style="font-size: 1.5em;text-transform: capitalize;font-weight: 600;">paid: <span>$38.76</span></div>
+                         </div>
+                 </div>
+                           
+
+                <div style="width:15%;float: right;margin:5px 10px 0px 10px">
+                <div style=" text-align: center;">
+                <div style="width: 7em;height: 5em;border: 2px solid black;position: flex;display: flex;justify-content: center;align-items: center;flex-direction: column;">
+                    <div style="font-size: 1em;">September</div>
+                    <div style="font-size: 1.5em; font-weight: 700;">01</div>
+                    <div style="font-size: 1em;">2021</div>
+                </div>
+                <div>
+                    <img src="<?php echo downloads_url() ?>ticket_6123940874306.png" alt="">
+                </div>
+            </div>
+                </div>
+
+
+            </div>
+            
+    </div>
+
+
+    <div style="border: .15em solid black;border-radius:10px;  width: 100%; position: relative; margin-bottom:20px; ">
+            <div style="display:inline-block; width:100%;">
+                <div style="width:35%;float: left;">
+                    <img src="<?php echo downloads_url() ?>gig8.jpg" alt="" style="width: 100%; margin: 5px;">
+                </div>
+                <div style="width: 45%; margin-left:20px; float: left;">
+                        <div>
+                            <div style="font-size: 2em;text-transform: uppercase;">Title of concert</div>
+                            <div style="font-size: 1em;text-transform: capitalize;">subtitle</div>
+                            <div style="font-size: 1em;text-transform: capitalize;">from <span style="font-weight: 600;text-transform: uppercase;">02:00 pm</span> to <span style="font-weight: 600;text-transform: uppercase;">04:00 pm</span></div>
+                         </div>  
+                         <div style="margin-top:30px;">
+                            <div style="font-size: 1.5em;text-transform: uppercase;font-weight: 600;">name of purchaser</div>
+                             <div style="font-size: 1.5em;text-transform: capitalize;font-weight: 600;">paid: <span>$38.76</span></div>
+                         </div>
+                 </div>
+                           
+
+                <div style="width:15%;float: right;margin:5px 10px 0px 10px">
+                <div style=" text-align: center;">
+                <div style="width: 7em;height: 5em;border: 2px solid black;position: flex;display: flex;justify-content: center;align-items: center;flex-direction: column;">
+                    <div style="font-size: 1em;">September</div>
+                    <div style="font-size: 1.5em; font-weight: 700;">01</div>
+                    <div style="font-size: 1em;">2021</div>
+                </div>
+                <div>
+                    <img src="<?php echo downloads_url() ?>ticket_6123940874306.png" alt="">
+                </div>
+            </div>
+                </div>
+
+
+            </div>
+            
+    </div>
+
+
+    <div style="border: .15em solid black;border-radius:10px;  width: 100%; position: relative; margin-bottom:20px; ">
+            <div style="display:inline-block; width:100%;">
+                <div style="width:35%;float: left;">
+                    <img src="<?php echo downloads_url() ?>gig8.jpg" alt="" style="width: 100%; margin: 5px;">
+                </div>
+                <div style="width: 45%; margin-left:20px; float: left;">
+                        <div>
+                            <div style="font-size: 2em;text-transform: uppercase;">Title of concert</div>
+                            <div style="font-size: 1em;text-transform: capitalize;">subtitle</div>
+                            <div style="font-size: 1em;text-transform: capitalize;">from <span style="font-weight: 600;text-transform: uppercase;">02:00 pm</span> to <span style="font-weight: 600;text-transform: uppercase;">04:00 pm</span></div>
+                         </div>  
+                         <div style="margin-top:30px;">
+                            <div style="font-size: 1.5em;text-transform: uppercase;font-weight: 600;">name of purchaser</div>
+                             <div style="font-size: 1.5em;text-transform: capitalize;font-weight: 600;">paid: <span>$38.76</span></div>
+                         </div>
+                 </div>
+                           
+
+                <div style="width:15%;float: right;margin:5px 10px 0px 10px">
+                <div style=" text-align: center;">
+                <div style="width: 7em;height: 5em;border: 2px solid black;position: flex;display: flex;justify-content: center;align-items: center;flex-direction: column;">
+                    <div style="font-size: 1em;">September</div>
+                    <div style="font-size: 1.5em; font-weight: 700;">01</div>
+                    <div style="font-size: 1em;">2021</div>
+                </div>
+                <div>
+                    <img src="<?php echo downloads_url() ?>ticket_6123940874306.png" alt="">
+                </div>
+            </div>
+                </div>
+
+
+            </div>
+            
+    </div>
+
+
+    <div style="border: .15em solid black;border-radius:10px;  width: 100%; position: relative; margin-bottom:20px; ">
+            <div style="display:inline-block; width:100%;">
+                <div style="width:35%;float: left;">
+                    <img src="<?php echo downloads_url() ?>gig8.jpg" alt="" style="width: 100%; margin: 5px;">
+                </div>
+                <div style="width: 45%; margin-left:20px; float: left;">
+                        <div>
+                            <div style="font-size: 2em;text-transform: uppercase;">Title of concert</div>
+                            <div style="font-size: 1em;text-transform: capitalize;">subtitle</div>
+                            <div style="font-size: 1em;text-transform: capitalize;">from <span style="font-weight: 600;text-transform: uppercase;">02:00 pm</span> to <span style="font-weight: 600;text-transform: uppercase;">04:00 pm</span></div>
+                         </div>  
+                         <div style="margin-top:30px;">
+                            <div style="font-size: 1.5em;text-transform: uppercase;font-weight: 600;">name of purchaser</div>
+                             <div style="font-size: 1.5em;text-transform: capitalize;font-weight: 600;">paid: <span>$38.76</span></div>
+                         </div>
+                 </div>
+                           
+
+                <div style="width:15%;float: right;margin:5px 10px 0px 10px">
+                <div style=" text-align: center;">
+                <div style="width: 7em;height: 5em;border: 2px solid black;position: flex;display: flex;justify-content: center;align-items: center;flex-direction: column;">
+                    <div style="font-size: 1em;">September</div>
+                    <div style="font-size: 1.5em; font-weight: 700;">01</div>
+                    <div style="font-size: 1em;">2021</div>
+                </div>
+                <div>
+                    <img src="<?php echo downloads_url() ?>ticket_6123940874306.png" alt="">
+                </div>
+            </div>
+                </div>
+
+
+            </div>
+            
+    </div>
+    
 </body>
 </html>
