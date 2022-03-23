56ec99d39cfabee774c28efb322d30ec099227de
sahahidsarvani <49752653+HamzaZafarBhatti@users.noreply.github.com>
2022-03-24 04:04:40 +0500

    Update Apis.php

--- application/controllers/Apis.php
+++ application/controllers/Apis.php
@@ -11,27 +11,27 @@ class Apis extends REST_Controller {
 		
 	} 
 	
-	public function verify_qr_code_by_token_get($ticket_qr_token){ 
-		if(strlen($ticket_qr_token) >0){
-			$row = $this->gigs_model->get_complete_ticket_detail_by_qr_token($ticket_qr_token); 
+	// public function verify_qr_code_by_token_get($ticket_qr_token){ 
+	// 	if(strlen($ticket_qr_token) >0){
+	// 		$row = $this->gigs_model->get_complete_ticket_detail_by_qr_token($ticket_qr_token); 
 			
-			if($row){ 
-				//$resp_arrs = array();  
-				//$resp_arrs[] = $row;
+	// 		if($row){ 
+	// 			//$resp_arrs = array();  
+	// 			//$resp_arrs[] = $row;
 				
-				$message = array( 'status' => "200", 'message' => "Ticket detail fetched successfully!", 'contents' => $row);
-				$this->set_response($message, REST_Controller::HTTP_OK);
-			}else{
-				$message = array( 'status' => "502", 'message' => "No record found!", 'contents' => []);
-				$this->set_response($message, REST_Controller::HTTP_NOT_FOUND); // NOT_FOUND (404) being the HTTP response code  
-			} 
+	// 			$message = array( 'status' => "200", 'message' => "Ticket detail fetched successfully!", 'contents' => $row);
+	// 			$this->set_response($message, REST_Controller::HTTP_OK);
+	// 		}else{
+	// 			$message = array( 'status' => "502", 'message' => "No record found!", 'contents' => []);
+	// 			$this->set_response($message, REST_Controller::HTTP_NOT_FOUND); // NOT_FOUND (404) being the HTTP response code  
+	// 		} 
 			
 			
-		}else{
-			$message = array( 'status' => "502", 'message' => "Invalid access, please provide QR Code token number!", 'contents' => []);
-			$this->set_response($message, REST_Controller::HTTP_NOT_FOUND); // NOT_FOUND (404) being the HTTP response code  
-		}
-	}  
+	// 	}else{
+	// 		$message = array( 'status' => "502", 'message' => "Invalid access, please provide QR Code token number!", 'contents' => []);
+	// 		$this->set_response($message, REST_Controller::HTTP_NOT_FOUND); // NOT_FOUND (404) being the HTTP response code  
+	// 	}
+	// }  
 	public function verify_qr_code_by_token($ticket_qr_token){ 
 		if(strlen($ticket_qr_token) >0){
 			$data = ['is_validated' => 1];
