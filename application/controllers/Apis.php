2ae663085e83e463b93c5894803efd150deb1e5a
sahahidsarvani <49752653+HamzaZafarBhatti@users.noreply.github.com>
2022-03-24 04:07:17 +0500

    ad

--- application/controllers/Apis.php
+++ application/controllers/Apis.php
@@ -11,34 +11,15 @@ class Apis extends REST_Controller {
 		
 	} 
 	
-	// public function verify_qr_code_by_token_get($ticket_qr_token){ 
-	// 	if(strlen($ticket_qr_token) >0){
-	// 		$row = $this->gigs_model->get_complete_ticket_detail_by_qr_token($ticket_qr_token); 
-			
-	// 		if($row){ 
-	// 			//$resp_arrs = array();  
-	// 			//$resp_arrs[] = $row;
-				
-	// 			$message = array( 'status' => "200", 'message' => "Ticket detail fetched successfully!", 'contents' => $row);
-	// 			$this->set_response($message, REST_Controller::HTTP_OK);
-	// 		}else{
-	// 			$message = array( 'status' => "502", 'message' => "No record found!", 'contents' => []);
-	// 			$this->set_response($message, REST_Controller::HTTP_NOT_FOUND); // NOT_FOUND (404) being the HTTP response code  
-	// 		} 
-			
-			
-	// 	}else{
-	// 		$message = array( 'status' => "502", 'message' => "Invalid access, please provide QR Code token number!", 'contents' => []);
-	// 		$this->set_response($message, REST_Controller::HTTP_NOT_FOUND); // NOT_FOUND (404) being the HTTP response code  
-	// 	}
-	// }  
-	public function verify_qr_code_by_token($ticket_qr_token){ 
+	public function verify_qr_code_by_token_get($ticket_qr_token){ 
 		if(strlen($ticket_qr_token) >0){
-			$data = ['is_validated' => 1];
-			$row = $this->gigs_model->update_tickets_data_by_qr_token($ticket_qr_token, $data); 
+			$row = $this->gigs_model->get_complete_ticket_detail_by_qr_token($ticket_qr_token); 
 			
 			if($row){ 
-				$message = array( 'status' => "200", 'message' => "Ticket is validated!", 'contents' => $row);
+				//$resp_arrs = array();  
+				//$resp_arrs[] = $row;
+				
+				$message = array( 'status' => "200", 'message' => "Ticket detail fetched successfully!", 'contents' => $row);
 				$this->set_response($message, REST_Controller::HTTP_OK);
 			}else{
 				$message = array( 'status' => "502", 'message' => "No record found!", 'contents' => []);
@@ -51,4 +32,23 @@ class Apis extends REST_Controller {
 			$this->set_response($message, REST_Controller::HTTP_NOT_FOUND); // NOT_FOUND (404) being the HTTP response code  
 		}
 	}  
+	// public function verify_qr_code_by_token_get($ticket_qr_token){ 
+	// 	if(strlen($ticket_qr_token) >0){
+	// 		$data = ['is_validated' => 1];
+	// 		$row = $this->gigs_model->update_tickets_data_by_qr_token($ticket_qr_token, $data); 
+			
+	// 		if($row){ 
+	// 			$message = array( 'status' => "200", 'message' => "Ticket is validated!", 'contents' => $row);
+	// 			$this->set_response($message, REST_Controller::HTTP_OK);
+	// 		}else{
+	// 			$message = array( 'status' => "502", 'message' => "No record found!", 'contents' => []);
+	// 			$this->set_response($message, REST_Controller::HTTP_NOT_FOUND); // NOT_FOUND (404) being the HTTP response code  
+	// 		} 
+			
+			
+	// 	}else{
+	// 		$message = array( 'status' => "502", 'message' => "Invalid access, please provide QR Code token number!", 'contents' => []);
+	// 		$this->set_response($message, REST_Controller::HTTP_NOT_FOUND); // NOT_FOUND (404) being the HTTP response code  
+	// 	}
+	// }  
 }
--- application/models/admin/Gigs_model.php
+++ application/models/admin/Gigs_model.php
@@ -432,7 +432,7 @@ class Gigs_model extends CI_Model
 	function get_complete_ticket_detail_by_qr_token($qr_token)
 	{
 
-		$query = $this->db->query("SELECT t1.id as ticket_id, t1.gig_id, t1.ticket_no, t1.qr_token, t2.title, t2.subtitle, t2.category, t2.poster, t2.address, t2.venues, t3.fname, t3.lname, t3.email, t4.created_on, t5.price, t5.is_paid FROM tickets t1
+		$query = $this->db->query("SELECT t1.id as ticket_id, t1.gig_id, t1.ticket_no, t1.qr_token, t2.title, t2.subtitle, t2.category, t2.poster, t2.address, t2.venues, t3.fname, t3.lname, t3.email, t4.created_on, t4.price, t5.is_paid FROM tickets t1
 		LEFT JOIN gigs t2 ON t2.id = t1.gig_id 
 		LEFT JOIN users t3 ON t3.id = t1.user_id 
 		LEFT JOIN ticket_tiers t4 ON t4.id = t1.ticket_tier_id  
