a1f5a380b664635100be507059b3e0fb588102e8
sahahidsarvani <49752653+HamzaZafarBhatti@users.noreply.github.com>
2022-03-24 02:40:42 +0500

    savd

--- application/config/email.php
+++ application/config/email.php
@@ -9,28 +9,28 @@ defined('BASEPATH') or exit('No direct script access allowed');
 |
 |
 */
-// $config = array(
-// 	'protocol' => 'smtp',
-// 	'smtp_host' => 'mail.dp8staging.com',
-// 	'smtp_port' => 465,
-// 	'smtp_user' => 'test@dp8staging.com',
-// 	'smtp_pass' => 'l3E1BN&?aLM6',
-// 	"smtp_crypto" => "ssl",
-// 	"smtp_timeout" => "10",
-// 	'crlf' => "\r\n",
-// 	'newline' => "\r\n",
-// );
 $config = array(
 	'protocol' => 'smtp',
-	'smtp_host' => 'gigniter.ca',
+	'smtp_host' => 'mail.dp8staging.com',
 	'smtp_port' => 465,
-	'smtp_user' => 'mail@gigniter.ca',
-	'smtp_pass' => '0s1ot4Y$',
+	'smtp_user' => 'test@dp8staging.com',
+	'smtp_pass' => 'l3E1BN&?aLM6',
 	"smtp_crypto" => "ssl",
 	"smtp_timeout" => "10",
 	'crlf' => "\r\n",
 	'newline' => "\r\n",
 );
+// $config = array(
+// 	'protocol' => 'smtp',
+// 	'smtp_host' => 'gigniter.ca',
+// 	'smtp_port' => 465,
+// 	'smtp_user' => 'mail@gigniter.ca',
+// 	'smtp_pass' => '0s1ot4Y$',
+// 	"smtp_crypto" => "ssl",
+// 	"smtp_timeout" => "10",
+// 	'crlf' => "\r\n",
+// 	'newline' => "\r\n",
+// );
 $config['mailtype'] = 'html';
 $config['wordwrap'] = 'true';
 
